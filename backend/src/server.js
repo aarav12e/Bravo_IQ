e1ba0bc10d349e841beb723e752e547629803c98
aarav12e <aarav12f@gmail.com>
2026-01-22 23:13:43 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,29 @@
 import exporess from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
 
 
 const app = exporess();
+
+const __dirname= path.resolve();
 
 console.log(ENV.PORT);
 console.log(ENV.DB_URL)
 
 app.get('/health', (req, res) => {
     res.status(200).json({ message: 'API is running..' });
-})
+});
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'this is boobs endpoint' });
+});
+
+//make our app ready for development
+if(ENV.NODE_ENV === 'production'){
+    app.use(exporess.static(path.join(__dirname, '../frontend/build')));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    });
+
+}
 
 app.listen(ENV.PORT, () => console.log('Server running on port:', ENV.PORT));
